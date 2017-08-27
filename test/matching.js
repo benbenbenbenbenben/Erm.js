6c06301ea61a7c1d7364e395bff5969069959485
Benjamin Babik <work@benjaminbabik.com>
2017-08-28 00:54:38 +0100

    docs (TODO: ignore outputs)

--- test/matching.js
+++ test/matching.js
@@ -4,14 +4,17 @@ const { match, make, not, _ } = require('../').Match
 
 describe("Match(...'hello world')", () => {
 
-  let h = make(p => p == 'h')
-  let l = make(p => p == 'l')
-  let z = make(p => p == 'z')
+  let h = p => p == 'h'
+  let e = p => p == 'e'
+  let l = p => p == 'l'
+  let o = p => p == 'o'
+  let z = p => p == 'z'
+  let hello = (ph,pe,pl,pL,po) => h(ph) && e(pe) && l(pl) && l(pL) && o(po)
 
   it(`should match once for: ${h}`, () => {
     let count = 0
     match(...'hello world')(
-      h(result => count++),
+      make(h)(result => count++),
       _
     )
     assert.equal(count, 1)
@@ -20,7 +23,7 @@ describe("Match(...'hello world')", () => {
   it(`should match thrice for: ${l}`, () => {
     let count = 0
     match(...'hello world')(
-      l(result => count++),
+      make(l)(result => count++),
       _
     )
     assert.equal(count, 3)
@@ -29,9 +32,22 @@ describe("Match(...'hello world')", () => {
   it(`should match never for: ${z}`, () => {
     let count = 0
     match(...'hello world')(
-      z(result => count++),
+      make(z)(result => count++),
       _
     )
     assert.equal(count, 0)
   })
+
+  it(`should match 3 contingent letters 'hel'`, () => {
+    let expected = [{start: 0, length: 1}, {start:1, length: 1}, {start:2, length:1}]
+    let actual = []
+    match(...'hello world')(
+      make(h)(result => {if(actual.length < 3) actual.push(result.location)}),
+      make(e)(result => {if(actual.length < 3) actual.push(result.location)}),
+      make(l)(result => {if(actual.length < 3) actual.push(result.location)}),
+      _
+    )
+    assert.deepEqual(expected, actual)
+  })
+
 })
